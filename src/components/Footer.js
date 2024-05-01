48f7e79a255eab31e709dda92461d777755d79b6
leoseis <eboscrib2@yahoo.com>
2024-05-01 16:50:17 +0100

    first components for header.js and footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div>
+      <footer> This is the footer</footer>
+    </div>
+  )
+}
+
+export default footer
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+      <header> This the header</header>
+    </div>
+  )
+}
+
+export default header
