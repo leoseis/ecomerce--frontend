9f466c82bee6c2ea8e1db13bae15b8c72a1cbfbf
leoseis <eboscrib2@yahoo.com>
2024-05-06 13:49:47 +0100

    HomeScreen page created and rendered

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./components/screens/HomeScreen";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main>
         <Container>
-          <h1> Welcome to my shop </h1>
+        <HomeScreen/>
         </Container>
       </main>
       <Footer />
---
+++ src/components/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import products from '../../product'
+import { Row,Col } from 'react-bootstrap'
+
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1 className='text-center'>Latest products</h1>
+      <Row>
+
+      </Row>
+    </div>
+  )
+}
+
+export default HomeScreen
