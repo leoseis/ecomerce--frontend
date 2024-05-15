cae080d49e4a69f83f8b82ebf35015af6d419b68
leoseis <eboscrib2@yahoo.com>
2024-05-15 17:24:51 +0100

    rendered more stars by duplication

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -12,7 +12,34 @@ function Rating({value,text,color}) {
   }
 ></i>
       </spam>
+      <spam>
+      <i
+  style={{ color }}
+  className={
+    value >= 2 ? 'fas fa-star' // solid star
+      : value >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star' // half star or empty star
+  }
+></i>
+      </spam>
       
+      <spam>
+      <i
+  style={{ color }}
+  className={
+    value >= 3 ? 'fas fa-star' // solid star
+      : value >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star' // half star or empty star
+  }
+></i>
+      </spam>
+      <spam>
+      <i
+  style={{ color }}
+  className={
+    value >= 4 ? 'fas fa-star' // solid star
+      : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star' // half star or empty star
+  }
+></i>
+      </spam>
     </div>
   )
 }
