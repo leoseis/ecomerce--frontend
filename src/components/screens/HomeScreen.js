8a48ec96781765c363a8c829c5a7a9f8c8c340b0
leoseis <eboscrib2@yahoo.com>
2024-06-08 20:22:23 +0100

    renderd from the api/8000 port

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import products from '../../product'
+import React, { useState, useEffect } from 'react';
+// import products from '../../product'
 import { Row,Col } from 'react-bootstrap'
 import Product from '../Product'
+import axios from 'axios'
 
 
 function HomeScreen() {
+  const [products, setProducts]= useState([])
+  useEffect(()=>{
+    async function fetchProducts(){
+      const {data}=await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+      fetchProducts()
+  },[])
+
+
   return (
     <div>
       <h1 className='text-center'>Latest products</h1>
